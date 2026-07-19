66cbb33149dcf279377d8b77143cc92fb2dc987c
Warren Fair <warren.l.fair@gmail.com>
2026-07-18 22:11:25 -0400

    Connect BF Outdoors to separate Supabase database

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://tmfyjzqghxntprexonrg.supabase.co";
-const SUPABASE_KEY = "sb_publishable_wYrEHjC8nWZgnjhT36fcDw_lp8hWH6c";
+const SUPABASE_URL = "https://mvrpposyhosxtosiwmeq.supabase.co";
+const SUPABASE_KEY = "sb_publishable_vfZNGN2RjQk5fd0_Y9KJIQ_qlTmFdkD";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
